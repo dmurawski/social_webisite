632165721b09a4ce0a62d94e758ddd619515878a
Damian Murawski <dmurawski018@gmail.com>
2024-12-21 19:10:49 +0100

    add reset, change password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # path("login/", views.user_login, name="login"),
-    # login / logout urls
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.dashboard, name="dashboard"),
+    path("", include("django.contrib.auth.urls")),
 ]
