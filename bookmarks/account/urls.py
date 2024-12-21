13830768151a6b128171bc87cf4c226b88e8099b
Damian Murawski <dmurawski018@gmail.com>
2024-12-21 20:02:50 +0100

    add register account

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
