ba540b2548d9cd7f4e540d4afd92e2fbb78cb946
Damian Murawski <dmurawski018@gmail.com>
2024-12-27 23:16:00 +0100

    add notifications to edit profile

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -79,6 +80,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            # django messages
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
