4ab451bb87dd4fd8df7a931882b9b15735ea4ff3
Damian Murawski <dmurawski018@gmail.com>
2024-12-30 00:21:52 +0100

    fix bug

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -48,7 +48,7 @@ def register(request):
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data["password"])
-        new_user.save()
+            new_user.save()
         # create profile for user
         Profile.objects.create(user=new_user)
         return render(
