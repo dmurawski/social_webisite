4416ffa78fa4ac4bb2b64b493bce2a154ca006ed
Damian Murawski <dmurawski018@gmail.com>
2024-12-30 23:47:06 +0100

    add user list,user detail and dynamically method to any user

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .forms import (
     LoginForm,
@@ -94,5 +94,34 @@ def edit(request):
         {
             "user_form": user_form,
             "profile_form": profile_form,
+        },
+    )
+
+
+User = get_user_model()
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request,
+        "account/user/list.html",
+        {
+            "section": "people",
+            "users": users,
+        },
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request,
+        "account/user/detail.html",
+        {
+            "section": "people",
+            "user": user,
         },
     )
