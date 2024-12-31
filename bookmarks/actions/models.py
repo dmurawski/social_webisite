a6483396c04eb508b587ac18973647de4835dcc3
Damian Murawski <dmurawski018@gmail.com>
2024-12-31 15:22:44 +0100

    add generic relation

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
-class Actions(models.Model):
+class Action(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="actions",
@@ -10,9 +12,22 @@ class Actions(models.Model):
     )
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    # generic relation
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name="target_obj",
+        on_delete=models.CASCADE,
+    )
+    # storing the primary key of the related object
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    # field to the related object based on the combination of the two previous fields
+    target = GenericForeignKey("target_ct", "target_id")
 
     class Meta:
         indexes = [
             models.Index(fields=["-created"]),
+            models.Index(fields=["target_ct", "target_id"]),
         ]
         ordering = ["-created"]
