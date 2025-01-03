5c6bbce5441231ee626792b8d1658b0847e2adac
Damian Murawski <dmurawski018@gmail.com>
2025-01-03 23:48:27 +0100

    add django debug toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -32,12 +32,14 @@ INSTALLED_APPS = [
     "social_django",
     "django_extensions",
     "easy_thumbnails",
+    "debug_toolbar",
     # local apps
     "images.apps.ImagesConfig",
     "actions.apps.ActionsConfig",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -164,3 +166,8 @@ SOCIAL_AUTH_PIPELINE = [
 ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
 }
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
